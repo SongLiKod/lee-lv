1f365b1b95ddc1ab9e03948133c90dd2fd0afa46
SongLiKod <1321089229@qq.com>
2023-07-29 13:01:35 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'c1534ef1-ab7f-4cb5-bd95-b4d1646d4003';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
